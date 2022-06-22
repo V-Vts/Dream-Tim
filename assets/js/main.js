c8de09f27c4a5b3e4a1d7ac6e33c66c8f0bfa96c
V-Vts <Vianney-Vts@users.noreply.github.com>
2022-06-22 14:57:17 +0200

    toutes les pages après réu 09/06

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,6 +146,92 @@
 
 
 // navbar
+class MyNavbar extends HTMLElement {
+	connectedCallback(){
+		this.innerHTML = 
+		`
+		<nav class="navbar">
+			<a class="navbar-logo" href="/presentation.html">
+				<img src="img/velo-logo.png" alt="Logo_DreamTim" class="logo">
+				<img src="img/texte-Dream-Tim.png" alt="Logo_DreamTim" class="text-logo">
+			</a>
+			<a style="cursor: pointer;" class="toggle-button">
+			<span class="bar"></span>
+			<span class="bar"></span>
+			<span class="bar"></span>
+			</a>
+			<div class="navbar-links">
+				<ul class="shown">
+					<li class="dropdown">
+						<!-- <i class="fa fa-angle-down" style="cursor: pointer;" onclick="location.href='/generic.html';"></i> -->
+						<!-- <span onclick="location.href='';" style="cursor: pointer;" href="" class="test"></span> -->
+						<a class="dropbtn">Le projet</a>
+						<ul class="dropdown-content">
+							<li><a href="/presentation.html">Présentation</a></li>
+							<li><a href="/aventurier.html">L'aventurier</a></li>
+							<li><a href="/itineraire.html">L'itinéraire</a></li>
+							<li><a href="/association.html">L'association</a></li>
+							<li><a href="/ecoles.html">Les écoles</a></li>
+							<li><a href="/staff.html">Le staff</a></li>
+						</ul>							
+					</li>
+					<li class="dropdown">
+						<!-- <span onclick="location.href='';" style="cursor: pointer;" href="" class="test"></span> -->
+						<a class="dropbtn">Actualités</a>
+						<ul class="dropdown-content">
+							<li><a href="/suiviGPS.html">Suivi GPS</a></li>
+						</ul>							
+					</li>
+					<li class="dropdown">
+						<!-- <span onclick="location.href='';" style="cursor: pointer;" href="" class="test"></span> -->
+						<a class="dropbtn">Rêves</a>
+						<ul class="dropdown-content">
+							<li><a href="/reveurs.html">Les rêveurs</a></li>
+							<li><a href="/1000et1.html">1000 et 1</a></li>
+						</ul>							
+					</li>
+					<li class="dropdown">
+						<!-- <span onclick="location.href='';" style="cursor: pointer;" href="" class="test"></span> -->
+						<a class="dropbtn">A propos</a>
+						<ul class="dropdown-content">
+							<li><a href="/partenaires.html">Partenaires</a></li>
+							<li><a href="/presse.html">Presse</a></li>
+							<li><a href="/contacts.html">Contacts</a></li>
+						</ul>							
+					</li>
+					<li><a class="don" href="https://www.petitsprinces.com/" target="_blank">Faire un don</a></li>
+				</ul>
+			</div>
+		</nav>
+		`
+	}
+}
+customElements.define('my-navbar',MyNavbar)
+
+//footer
+class MyFooter extends HTMLElement {
+	connectedCallback(){
+		this.innerHTML = 
+		`<section id="footer" class="wrapper style7">
+			<ul class="icons">
+				<li><a href="https://www.instagram.com/dreamtim___/" target="_blank" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+				<li><a href="https://www.facebook.com/DREAM-TIM-101018092510498" target="_blank" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+				<li><a href="https://www.linkedin.com/company/dreamtimm/" target="_blank" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+				<li><a href="https://www.youtube.com/channel/UChdjiYkner2JQWavSM1i-3A" target="_blank" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
+				<li><a href="mailto:contact.dreamtim@gmail.com" target="_blank" class="icon solid fa-envelope"><span class="label">Mail</span></a></li>
+			</ul>
+			<div class="copyright">
+				<ul class="menu">
+					<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="https://www.linkedin.com/in/vianney-vouters/" target="_blank">Vianney Vts</a></li>
+				</ul>
+			</div>
+		</section>
+		`
+	}
+}
+customElements.define('my-footer',MyFooter)
+
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
@@ -190,10 +276,7 @@ for(let i=0; i<dropdown.length; i++){
 
 $(document).click(function(event) { 
 	var target = event.target;
-	console.log(target);
-	console.log(target.closest('.dropdown'));
 	if(target.closest('.shown') == null) {
-		console.log('test');
 		for(let j=0; j<dropdown.length; j++){
 			dropdownContent[j].classList.remove('active');
 		}
@@ -201,18 +284,121 @@ $(document).click(function(event) {
   });
 
   // compte à rebours
-  var deadline = new Date("Jul 5, 2022 12:00:00").getTime();
-  var x = setInterval(function() {
-  var now = new Date().getTime();
-  var t = deadline - now;
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((t % (1000 * 60)) / 1000);
-  document.getElementById("decompte").innerHTML = days + " jours " 
-  + hours + " h " + minutes + " m " + seconds + " s ";
-	  if (t < 0) {
-		  clearInterval(x);
-		  document.getElementById("decompte").innerHTML = "EXPIRED";
-	  }
-  }, 1000);
+  if (document.getElementById("decompte") != null) {
+	var deadline = new Date("Sep 10, 2022 12:00:00").getTime();
+	var x = setInterval(function() {
+	  var now = new Date().getTime();
+	  var t = deadline - now;
+	  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+	  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((t % (1000 * 60)) / 1000);
+	  document.getElementById("decompte").innerHTML = days + " jours " 
+	  + hours + " h " + minutes + " m " + seconds + " s ";
+		  if (t < 0) {
+			  clearInterval(x);
+			  document.getElementById("decompte").innerHTML = "EXPIRED";
+		  }
+	  }, 1000);
+  }
+
+  // slider
+  let slidePosition = [];
+  let totalSlides = [];
+  const swipers = document.getElementsByClassName('swiper');
+  const slides = []
+  let nbSwipers = -1
+
+  for (let swiper of swipers) {
+	nbSwipers++;
+	slides.push(swiper.getElementsByClassName('slide'));
+	slidePosition.push(0);
+  	totalSlides.push(slides[nbSwipers].length);
+}
+
+  function updateSlidePosition(indexSwiper) {
+	for (let slide of slides[indexSwiper]) {
+	  slide.classList.remove('slide-visible');
+	  slide.classList.add('slide-hidden');
+	}
+	slides[indexSwiper][slidePosition[indexSwiper]].classList.remove('slide-hidden');
+	slides[indexSwiper][slidePosition[indexSwiper]].classList.add('slide-visible');
+  }
+
+function moveToNextSlide(indexSwiper) {
+	console.log('index',indexSwiper)
+	if (slidePosition[indexSwiper] === totalSlides[indexSwiper] - 1) {
+	  slidePosition[indexSwiper] = 0;
+	} else {
+	  slidePosition[indexSwiper]++;
+	}
+	updateSlidePosition(indexSwiper);
+}
+  
+function moveToPrevSlide(indexSwiper) {
+	if (slidePosition[indexSwiper] === 0) {
+	  slidePosition[indexSwiper] = totalSlides[indexSwiper] - 1;
+	} else {
+	  slidePosition[indexSwiper]--;
+	}
+  
+	updateSlidePosition(indexSwiper);
+}
+
+document.addEventListener('click',function (event) {
+	if (event.target.matches('button')) {
+		if (event.target.matches('.swiper-button-prev')) {
+			let swiperNb = parseInt(event.target.className.split(' ')[1]);
+			moveToPrevSlide(swiperNb);
+		}
+		if (event.target.matches('.swiper-button-next')) {
+			let swiperNb = parseInt(event.target.className.split(' ')[1]);
+			moveToNextSlide(swiperNb);
+		}
+	}
+}, false);
+
+
+
+//   const slides = document.getElementsByClassName('slide');
+
+//   document.
+// 	getElementById('swiper-button-next')
+// 	.addEventListener("click", function() {
+// 	  moveToNextSlide();
+// 	});
+//   document.
+// 	getElementById('swiper-button-prev')
+// 	.addEventListener("click", function() {
+// 	  moveToPrevSlide();
+// 	});
+  
+//   function updateSlidePosition() {
+// 	for (let slide of slides) {
+// 	  slide.classList.remove('slide-visible');
+// 	  slide.classList.add('slide-hidden');
+// 	}
+// 	slides[slidePosition].classList.remove('slide-hidden');
+// 	slides[slidePosition].classList.add('slide-visible');
+//   }
+  
+//   function moveToNextSlide() {
+// 	if (slidePosition === totalSlides - 1) {
+// 	  slidePosition = 0;
+// 	} else {
+// 	  slidePosition++;
+// 	}
+// 	updateSlidePosition();
+//   }
+  
+//   function moveToPrevSlide() {
+// 	if (slidePosition === 0) {
+// 	  slidePosition = totalSlides - 1;
+// 	} else {
+// 	  slidePosition--;
+// 	}
+  
+// 	updateSlidePosition();
+//   }
+
+//   updateSlidePosition();
